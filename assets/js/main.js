5d4e988ab56781bd80614007dd6f91e0bcc7cdf1
Andrea Marangon <marangonandrea84@gmail.com>
2021-04-12 12:12:44 +0200

    aggiunta di bonus post-correzione

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,37 @@ Vue.config.devtools = true;
 var app = new Vue({
   el: '#root',
   data: {
-    disks: []
+    disks: [],
+    genreFilter: '',
+    genreOptions:[],
+    orderDesc: true
+
   },
   mounted(){
-    for (var i = 0; i < 10; i++) {
       axios.get('https://flynn.boolean.careers/exercises/api/array/music')
        .then((result) => {
          this.disks = result.data.response;
+
+       this.disks.forEach((disk) => {
+         if(!this.genreOptions.includes(disk.genre)) {
+           this.genreOptions.push(disk.genre);
+         }
+       });
+
       });
 
-    }
-  },
 
+  },
+  methods:{
+    yearSort: function () {
+        this.orderDesc = !this.orderDesc;
+        if (this.orderDesc) {
+          this.disks.sort((a, b) => b.year - a.year);
+          }else {
+            this.disks.sort((a, b) => a.year - b.year);
+          }
+
+  }
+
+  }
 });
